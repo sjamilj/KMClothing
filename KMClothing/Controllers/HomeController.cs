65316e1bd4dcaaa20b08f199da3b00a93bed9017
sjamilj <shafayatjamil12@gmail.com>
2022-08-28 00:44:39 +0600

    second commit

--- KMClothing/Controllers/HomeController.cs
+++ KMClothing/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using KMClothing.Models.Home;
+﻿using KMClothing.DAL;
+using KMClothing.Models.Home;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,25 +10,108 @@ namespace KMClothing.Controllers
 {
     public class HomeController : Controller
     {
-        
-        public ActionResult Index()
+        dbKMClothingEntities ctx = new dbKMClothingEntities();
+
+        public ActionResult Index(string search,int? page)
         {
             HomeIndexViewModel model = new HomeIndexViewModel();
-            return View(model.CreateModel());
+            return View(model.CreateModel(search,4,page));
         }
 
-        public ActionResult About()
+        public ActionResult Checkout()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        public ActionResult CheckoutDetails()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        public ActionResult DecreaseQty(int productId)
+        {
+            if (Session["cart"] != null)
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        if (prevQty > 0)
+                        {
+                            cart.Remove(item);
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = prevQty - 1
+                            });
+                        }
+                        break;
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Checkout");
+        }
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var count = cart.Count();
+                var product = ctx.Tbl_Product.Find(productId);
+                for (int i = 0; i < count; i++)
+                {
+                    if (cart[i].Product.ProductId == productId)
+                    {
+                        int prevQty = cart[i].Quantity;
+                        cart.Remove(cart[i]);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                    }
+                }
+                Session["cart"] = cart;
+            }
+
+            return Redirect("Index");
+        }
+
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
         }
     }
 }
--- KMClothing/Models/Home/HomeIndexViewModel.cs
+++ KMClothing/Models/Home/HomeIndexViewModel.cs
@@ -2,21 +2,31 @@
 using KMClothing.Repository;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
 
 namespace KMClothing.Models.Home
 {
     public class HomeIndexViewModel
     {
         public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
-        public IEnumerable<Tbl_Product> ListOfProducts { get; set; }
-            public  HomeIndexViewModel CreateModel()
+        dbKMClothingEntities context =new dbKMClothingEntities();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+            public  HomeIndexViewModel CreateModel(string search,int pageSize,int?page)
             {
+            SqlParameter[] pram = new SqlParameter[] {
+                   new SqlParameter("@search",search??(object)DBNull.Value)
+                   };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch",pram).ToList().ToPagedList(page ?? 1, pageSize);
+
             return new HomeIndexViewModel
             {
-                ListOfProducts = _unitOfWork.GetRepositoryInstance<Tbl_Product>().GetAllRecords()
-            };
+                ListOfProducts = data
+             };
             }
     }
 }
